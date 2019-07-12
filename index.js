6e31be716e3b443473c8ae5593ee4c5f334ae2c8
Pilar Fernandez <pilarbfernandez28@gmail.com>
2019-07-12 08:17:03 -0300

    Ajustando para implementação da integração com communicator

---
+++ index.js
@@ -0,0 +1,10 @@
+/*
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+'use strict';
+
+const FoodContract = require('./lib/food-contract');
+
+module.exports.FoodContract = FoodContract;
+module.exports.contracts = [ FoodContract ];
